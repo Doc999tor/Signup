02c68ab9de8e01efd73d4dd07f1559eccd065b75
schvvarz <kirillsumovskyi@gmail.com>
2019-09-06 20:03:50 +0300

    loader click disabled

--- app/components/start-button/start-button.jsx
+++ app/components/start-button/start-button.jsx
@@ -4,7 +4,11 @@ import './start-button.less'
 const StartButton = ({route, active, isStartLoad}) => {
   return (
     <footer className='start-button'>&nbsp;
-      <button className={`all-set-form__button start ${active ? 'active' : ''}`} onClick={()=>route()} disabled={!active}>
+      <button className={`all-set-form__button start ${active ? 'active' : ''}`} onClick={()=>{
+        if (!isStartLoad) {
+          route()
+        }
+      }} disabled={!active}>
         <span className='start-button__all-set'>{_config.translations[_config.data.lang].all_set.lets_start}</span>
         {isStartLoad && <div className={`preloader ${_config.data.isRTL ? 'styleLoaderRTL' : 'styleLoaderLTR'}`}>
           <img className='loader' src={`${_config.urls.static}preloader.svg`} />
