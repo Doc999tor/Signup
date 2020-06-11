b606270bccc15c101736d34e38f39295cfbf8f3a
tkachuks <serhiytkachuk88@gmail.com>
2020-06-11 19:00:29 +0300

    fix click on input

--- app/pages/sign-up/sign-up.jsx
+++ app/pages/sign-up/sign-up.jsx
@@ -83,57 +83,59 @@ class SignUp extends Component {
     const { validPhone, sending, send } = this.state
     return (
       <div className='sign-up'>
-        <div style={{ backgroundImage: 'linear-gradient(123deg, #591ec0, #6623db 28%, #7d3ee8 54%, #be95ff 113%)' }} className='bottom_bgr'>
-          <img className='wave' src={`${_config.urls.static}wave.svg`} alt='wave' />
-          <div className='logo-wrap'>
-            <a href={_config.urls.home} className='home-link'>
-              <img className='sign-up-logo' src={`${_config.urls.static}logo.svg`} />
-              <img className='sign-up-logo-name' src={`${_config.urls.static}atzma.im.svg`} />
-            </a>
-          </div>
-        </div>
-        {send
-          ? <SendModal sending={sending} />
-          : (
-            <div className='sign-up-wrap'>
-              <div className='title-container'>
-                <h1>{_config.translations[_config.data.lang].sign_up.main_title}</h1>
-              </div>
-              <div className='question-container'>
-                <p className='sign-fill_fields'>{_config.translations[_config.data.lang].sign_up.fill_fields_label}</p>
-                <p className='sign-up-question'><span>{_config.translations[_config.data.lang].sign_up.have_acc_alredy}</span><a href={window.location.origin + _config.urls.login}>{_config.translations[_config.data.lang].sign_up.login_in}</a></p>
-              </div>
-              <form>
-                <div className='text-content-wrap'>
-                  <div className={`group${this.state.validName ? '' : ' err'}`}>
-                    <img className='phone_img' src={`${_config.urls.static}ic_name.svg`} />
-                    <input
-                      type='text'
-                      name='name'
-                      value={this.state.name}
-                      className='group__input input_name'
-                      onChange={this.handleChangeName}
-                      placeholder={_config.translations[_config.data.lang].sign_in.enter_name}
-                    />
-                  </div>
-                  <div className={`group${validPhone ? '' : ' err_phone'}`}>
-                    <img className='phone_img' src={`${_config.urls.static}ic_phone.svg`} />
-                    <input
-                      type='tel'
-                      name='phone'
-                      value={this.state.phone}
-                      className='group__input input_phone'
-                      onChange={this.handleChangePhone}
-                      placeholder={_config.translations[_config.data.lang].sign_in.enter_phone}
-                    />
-                  </div>
-                </div>
-                <button className='login-form__button login-button' type='button' onClick={this.sendLead}>
-                  {_config.translations[_config.data.lang].sign_up.send}
-                </button>
-              </form>
+        <div className='main-content'>
+          <div style={{ backgroundImage: 'linear-gradient(123deg, #591ec0, #6623db 28%, #7d3ee8 54%, #be95ff 113%)' }} className='bottom_bgr'>
+            <img className='wave' src={`${_config.urls.static}wave.svg`} alt='wave' />
+            <div className='logo-wrap'>
+              <a href={_config.urls.home} className='home-link'>
+                <img className='sign-up-logo' src={`${_config.urls.static}logo.svg`} />
+                <img className='sign-up-logo-name' src={`${_config.urls.static}atzma.im.svg`} />
+              </a>
             </div>
-          )}
+          </div>
+          {send
+            ? <SendModal sending={sending} />
+            : (
+              <div className='sign-up-wrap'>
+                <div className='title-container'>
+                  <h1>{_config.translations[_config.data.lang].sign_up.main_title}</h1>
+                </div>
+                <div className='question-container'>
+                  <p className='sign-fill_fields'>{_config.translations[_config.data.lang].sign_up.fill_fields_label}</p>
+                  <p className='sign-up-question'><span>{_config.translations[_config.data.lang].sign_up.have_acc_alredy}</span><a href={window.location.origin + _config.urls.login}>{_config.translations[_config.data.lang].sign_up.login_in}</a></p>
+                </div>
+                <form>
+                  <div className='text-content-wrap'>
+                    <div className={`group${this.state.validName ? '' : ' err'}`}>
+                      <img className='phone_img' src={`${_config.urls.static}ic_name.svg`} />
+                      <input
+                        type='text'
+                        name='name'
+                        value={this.state.name}
+                        className='group__input input_name'
+                        onChange={this.handleChangeName}
+                        placeholder={_config.translations[_config.data.lang].sign_in.enter_name}
+                      />
+                    </div>
+                    <div className={`group${validPhone ? '' : ' err_phone'}`}>
+                      <img className='phone_img' src={`${_config.urls.static}ic_phone.svg`} />
+                      <input
+                        type='tel'
+                        name='phone'
+                        value={this.state.phone}
+                        className='group__input input_phone'
+                        onChange={this.handleChangePhone}
+                        placeholder={_config.translations[_config.data.lang].sign_in.enter_phone}
+                      />
+                    </div>
+                  </div>
+                  <button className='login-form__button login-button' type='button' onClick={this.sendLead}>
+                    {_config.translations[_config.data.lang].sign_up.send}
+                  </button>
+                </form>
+              </div>
+            )}
+        </div>
           <div className='call_now'>
             <p>{_config.translations[_config.data.lang].sign_up.call_now}</p>
           <a href={`tel:${_config.data.phone}`}>{_config.data.phone}</a>
