fd35f9f2b0fa623f5f47ee6bf72446c6ac676310
tkachuks <serhiytkachuk88@gmail.com>
2020-06-19 10:02:44 +0300

    align container in logo animation. fix style in login sentence

--- app/pages/sign-up/sign-up.jsx
+++ app/pages/sign-up/sign-up.jsx
@@ -120,7 +120,7 @@ class SignUp extends Component {
             <h1>{_config.translations[_config.data.lang].sign_up.main_title}</h1>
           </div>
           <div className='question-container'>
-            <p className='sign-up-question'><span>{_config.translations[_config.data.lang].sign_up.have_acc_alredy}</span><a href={window.location.origin + _config.urls.login} >{_config.translations[_config.data.lang].sign_up.login_in}</a></p>
+            <a href={window.location.origin + _config.urls.login} className='sign-up-question'><span>{_config.translations[_config.data.lang].sign_up.have_acc_alredy}</span><span className='login_label'>{_config.translations[_config.data.lang].sign_up.login_in}</span></a>
           </div>
           <form>
             <div className='text-content-wrap'>
